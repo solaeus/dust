fa6af4026b01d6d97bc7f8282f36adcba41b6221
Jeff <dev@jeffa.io>
2023-10-22 14:10:16 -0400

    Implementing insert

--- src/abstract_tree/insert.rs
+++ src/abstract_tree/insert.rs
@@ -11,7 +11,7 @@ pub struct Insert {
 
 impl AbstractTree for Insert {
     fn from_syntax_node(source: &str, node: Node) -> Result<Self> {
-        let identifier_node = node.child(1).unwrap();
+        let identifier_node = node.child(2).unwrap();
         let identifier = Identifier::from_syntax_node(source, identifier_node)?;
         let expression_node = node.child(3).unwrap();
         let expression = Expression::from_syntax_node(source, expression_node)?;
@@ -28,6 +28,8 @@ impl AbstractTree for Insert {
         let new_rows = self.expression.run(source, context)?.into_inner_list()?;
 
         table.reserve(new_rows.len());
+
+        println!("{new_rows:?}");
 
         for row in new_rows {
             table.insert(row.into_inner_list()?)?;
