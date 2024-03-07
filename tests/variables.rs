65d2fd3270b25692a7eb5bcb4b87fd46b20bc970
Jeff <dev@jeffa.io>
2024-03-06 23:21:07 -0500

    Add and pass test

--- tests/variables.rs
+++ tests/variables.rs
@@ -1,6 +1,32 @@
-use dust_lang::*;
+use dust_lang::{
+    abstract_tree::Type,
+    error::{Error, TypeCheckError, ValidationError},
+    *,
+};
 
 #[test]
 fn set_and_get_variable() {
     assert_eq!(interpret("foobar = true; foobar"), Ok(Value::boolean(true)));
+}
+
+#[test]
+fn set_variable_with_type() {
+    assert_eq!(
+        interpret("foobar: bool = true; foobar"),
+        Ok(Value::boolean(true))
+    );
+}
+
+#[test]
+fn set_variable_with_type_error() {
+    assert_eq!(
+        interpret("foobar: str = true"),
+        Err(vec![Error::Validation {
+            error: ValidationError::TypeCheck(TypeCheckError {
+                actual: Type::Boolean,
+                expected: Type::String
+            }),
+            span: (0..18).into()
+        }])
+    );
 }
