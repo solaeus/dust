827a3df815c027fff7bad9a1bb8a26f438899365
Jeff <dev@jeffa.io>
2024-12-03 14:27:48 -0500

    Add math tests

---
+++ dust-lang/tests/math_subtract.rs
@@ -0,0 +1,65 @@
+use dust_lang::*;
+
+#[test]
+fn subtract_floats() {
+    let source = "2.0 - 2.0";
+
+    assert_eq!(
+        compile(source),
+        Ok(Chunk::with_data(
+            None,
+            FunctionType {
+                type_parameters: None,
+                value_parameters: None,
+                return_type: Box::new(Type::Float),
+            },
+            vec![
+                (
+                    Instruction::subtract(
+                        Destination::Register(0),
+                        Argument::Constant(0),
+                        Argument::Constant(0)
+                    ),
+                    Span(4, 5)
+                ),
+                (Instruction::r#return(true), Span(9, 9)),
+            ],
+            vec![ConcreteValue::Float(2.0)],
+            vec![]
+        ))
+    );
+
+    assert_eq!(run(source), Ok(Some(ConcreteValue::Float(0.0))));
+}
+
+#[test]
+fn subtract_integers() {
+    let source = "1 - 2";
+
+    assert_eq!(
+        compile(source),
+        Ok(Chunk::with_data(
+            None,
+            FunctionType {
+                type_parameters: None,
+                value_parameters: None,
+                return_type: Box::new(Type::Integer),
+            },
+            vec![
+                (
+                    Instruction::subtract(
+                        Destination::Register(0),
+                        Argument::Constant(0),
+                        Argument::Constant(1)
+                    ),
+                    Span(2, 3)
+                ),
+                (Instruction::r#return(true), Span(5, 5)),
+            ],
+            vec![ConcreteValue::Integer(1), ConcreteValue::Integer(2)],
+            vec![]
+        ))
+    );
+
+    assert_eq!(run(source), Ok(Some(ConcreteValue::Integer(-1))));
+}
