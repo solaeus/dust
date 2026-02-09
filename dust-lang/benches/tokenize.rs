ca166b0dc67a02c2429041ecb2321eac9186131c
Jeff <dev@jeffa.io>
2026-02-09 16:19:06 -0500

    Clean up tokenize bench

--- dust-lang/benches/tokenize.rs
+++ dust-lang/benches/tokenize.rs
@@ -1,4 +1,4 @@
-#![feature(iterator_try_collect)]
+#![feature(iterator_try_collect, iter_intersperse)]
 
 use std::hint::black_box;
 
@@ -15,24 +15,30 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("tokenize");
     let all_ascii = Vec::from_iter(0u8..=127);
 
-    let all_ascii_10k = all_ascii.repeat(10_000);
-    group.throughput(Throughput::Bytes(all_ascii_10k.len() as u64));
-    group.bench_function("combined: ASCII x10k", |b| {
-        b.iter(|| tokenize(black_box(&all_ascii_10k)))
-    });
+    {
+        let all_ascii_10k = all_ascii.repeat(10_000);
 
-    let all_ascii_100k = all_ascii.repeat(100_000);
-    group.throughput(Throughput::Bytes(all_ascii_100k.len() as u64));
-    group.bench_function("combined: ASCII x100k", |b| {
-        b.iter(|| tokenize(black_box(&all_ascii_100k)))
-    });
+        group.throughput(Throughput::Bytes(all_ascii_10k.len() as u64));
+        group.bench_function("All ASCII x10k", |b| {
+            b.iter(|| tokenize(black_box(&all_ascii_10k)))
+        });
+    }
+
+    {
+        let all_ascii_100k = all_ascii.repeat(100_000);
+
+        group.throughput(Throughput::Bytes(all_ascii_100k.len() as u64));
+        group.bench_function("All ASCII x100k", |b| {
+            b.iter(|| tokenize(black_box(&all_ascii_100k)))
+        });
+    }
 
     let utf8_range = 0..=0x10FFFF;
     let surrogate_range = 0xD800..=0xDFFF;
     let mut bytes = [0u8; 4];
     let mut all_valid_utf8 = Vec::new();
 
-    for codepoint in utf8_range {
+    for codepoint in utf8_range.clone() {
         if surrogate_range.contains(&codepoint) {
             continue;
         }
@@ -43,33 +49,59 @@ fn criterion_benchmark(c: &mut Criterion) {
         all_valid_utf8.extend_from_slice(&bytes[..character.len_utf8()]);
     }
 
-    group.throughput(Throughput::Bytes(all_valid_utf8.len() as u64));
-    group.bench_function("combined: UTF-8 all", |b| {
-        b.iter(|| tokenize(black_box(&all_valid_utf8)))
-    });
+    {
+        group.throughput(Throughput::Bytes(all_valid_utf8.len() as u64));
+        group.bench_function("Full UTF-8 range", |b| {
+            b.iter(|| tokenize(black_box(&all_valid_utf8)))
+        });
+    }
 
-    let all_valid_utf8_5x = all_valid_utf8.repeat(5);
-    group.throughput(Throughput::Bytes(all_valid_utf8_5x.len() as u64));
-    group.bench_function("combined: UTF-8 all x5", |b| {
-        b.iter(|| tokenize(black_box(&all_valid_utf8_5x)))
-    });
+    {
+        let all_valid_utf8_5x = all_valid_utf8.repeat(5);
 
-    let mixed_bytes = all_ascii
-        .iter()
-        .chain(all_valid_utf8.iter())
-        .cloned()
-        .collect::<Vec<u8>>();
+        group.throughput(Throughput::Bytes(all_valid_utf8_5x.len() as u64));
+        group.bench_function("Full UTF-8 range x5", |b| {
+            b.iter(|| tokenize(black_box(&all_valid_utf8_5x)))
+        });
+    }
 
-    group.throughput(Throughput::Bytes(mixed_bytes.len() as u64));
-    group.bench_function("combined: Mixed x1", |b| {
-        b.iter(|| tokenize(black_box(&mixed_bytes)))
-    });
+    let mut all_ascii = all_ascii.into_iter().cycle();
+    let mut mixed_bytes = Vec::new();
 
-    let mixed_bytes_5x = mixed_bytes.repeat(5);
-    group.throughput(Throughput::Bytes(mixed_bytes_5x.len() as u64));
-    group.bench_function("combined: Mixed x5", |b| {
-        b.iter(|| tokenize(black_box(&mixed_bytes_5x)))
-    });
+    for (index, codepoint) in utf8_range.enumerate() {
+        if surrogate_range.contains(&codepoint) {
+            continue;
+        }
+
+        if index % 2 == 0 {
+            let ascii = all_ascii.next().or_else(|| all_ascii.next()).unwrap();
+
+            mixed_bytes.push(ascii);
+
+            continue;
+        }
+
+        let character = std::char::from_u32(codepoint).unwrap();
+
+        character.encode_utf8(&mut bytes);
+        mixed_bytes.extend_from_slice(&bytes[..character.len_utf8()]);
+    }
+
+    {
+        group.throughput(Throughput::Bytes(mixed_bytes.len() as u64));
+        group.bench_function("Mixed ASCII and non-ASCII x1", |b| {
+            b.iter(|| tokenize(black_box(&mixed_bytes)))
+        });
+    }
+
+    {
+        let mixed_bytes_5x = mixed_bytes.repeat(5);
+
+        group.throughput(Throughput::Bytes(mixed_bytes_5x.len() as u64));
+        group.bench_function("Mixed ASCII and non-ASCII x5", |b| {
+            b.iter(|| tokenize(black_box(&mixed_bytes_5x)))
+        });
+    }
 
     group.finish();
 }
