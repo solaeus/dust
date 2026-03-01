7a0eceacf9f2b56fad39b2b5fa122b897a6f67f8
Jeff <dev@jeffa.io>
2026-03-01 00:47:34 -0500

    Begin adding new tests

--- dust-lang/src/tests/struct_item.rs
+++ dust-lang/src/tests/struct_item.rs
@@ -1,3 +1,4 @@
 pub const EMPYT: &[u8] = b"struct Foo {}";
 pub const TUPLE: &[u8] = b"struct Foo(int, int);";
 pub const FIELDS: &[u8] = b"struct Foo { x: int, y: int }";
+pub const TYPE_PARAMETERS: &[u8] = b"struct Foo<A, B, C> {}";
